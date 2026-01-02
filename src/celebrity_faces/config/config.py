a913fb226c00c2f4e5f8ed727f8386b7ccd4c664
babaksoft <babak.eslamieh@gmail.com>
2026-01-02 11:33:38 +0330

    Add preprocessing pipeline using AI suggestions

--- src/celebrity_faces/config/config.py
+++ src/celebrity_faces/config/config.py
@@ -1,23 +1,31 @@
 from pathlib import Path
 
+import tensorflow as tf
+
 
 RANDOM_STATE = 147
-
 PROJECT_NAME = "celebrity-faces"
-PACKAGE_ROOT = Path(__file__).resolve().parent.parent
 
+# Project structure
+PACKAGE_ROOT = Path(__file__).resolve().parent.parent
 DATA_PATH = PACKAGE_ROOT / "data"
 MODEL_PATH = PACKAGE_ROOT / "model"
 METRICS_PATH = PACKAGE_ROOT / "metrics"
-
 IMAGE_ROOT = DATA_PATH / "raw" / "Celebrity Faces Dataset"
+
+# Selected classes and encoded labels
 LABELS = [
     "Angelina Jolie", "Kate Winslet", "Natalie Portman", "Nicole Kidman", "Sandra Bullock",
     "Brad Pitt", "Johnny Depp", "Leonardo DiCaprio", "Tom Cruise", "Tom Hanks"
 ]
 LABEL_MAP = { LABELS[idx]: idx for idx in range(len(LABELS)) }
-NEW_SIZE = (160, 160) # Final image size for preprocessing
 
+# Dataset splitting
 TRAIN_SPLIT = 0.7
 VAL_SPLIT = 0.15
 TEST_SPLIT = 0.15
+
+# Data processing
+IMAGE_SIZE = (160, 160)
+BATCH_SIZE = 8
+AUTOTUNE = tf.data.AUTOTUNE
--- src/celebrity_faces/pipeline.py
+++ src/celebrity_faces/pipeline.py
@@ -1,6 +1,118 @@
+from tensorflow.keras.utils \
+    import set_random_seed, image_dataset_from_directory
+from tensorflow.keras.layers import Rescaling, RandomRotation, RandomFlip
+from tensorflow.keras.models import Sequential
 
-def build_pipeline():
-    return 0
+from .config import config
 
 
-pipeline = build_pipeline()
+class Pipeline:
+    def __init__(self):
+        self._layers = {
+            "rescaling": Rescaling(1./255),
+            "augmentation": Sequential([
+                RandomRotation(0.1),
+                RandomFlip("horizontal")
+            ])
+        }
+
+        self.train_ds = self._train_pipeline()
+        self.val_ds = self._val_pipeline()
+        self.test_ds = self._test_pipeline()
+
+    def _train_pipeline(self):
+        # Load train set with shuffling
+        ds = image_dataset_from_directory(
+            config.DATA_PATH / "train",
+            labels="inferred",
+            label_mode="int",
+            class_names=config.LABELS,
+            batch_size=config.BATCH_SIZE,
+            image_size=config.IMAGE_SIZE,
+            shuffle=True,
+            crop_to_aspect_ratio=True,
+            data_format="channels_last",
+            verbose=False
+        )
+
+        # Rescale to achieve more stable convergence
+        ds = ds.map(
+            lambda x, y: (self._layers["rescaling"](x), y),
+            num_parallel_calls=config.AUTOTUNE
+        )
+
+        # Perform data augmentation, ONLY on train set, ONLY during training
+        ds = ds.map(
+            lambda x, y: (self._layers["augmentation"](x, training=True), y),
+            num_parallel_calls=config.AUTOTUNE
+        )
+
+        # Optimize performance by caching and prefetching
+        ds = ds.cache().shuffle(500).prefetch(config.AUTOTUNE)
+
+        return ds
+
+    def _val_pipeline(self):
+        # Load validation set without shuffling
+        ds = image_dataset_from_directory(
+            config.DATA_PATH / "validation",
+            labels="inferred",
+            label_mode="int",
+            class_names=config.LABELS,
+            batch_size=config.BATCH_SIZE,
+            image_size=config.IMAGE_SIZE,
+            shuffle=False,
+            crop_to_aspect_ratio=True,
+            data_format="channels_last",
+            verbose=False
+        )
+
+        # Rescale to achieve more stable convergence
+        ds = ds.map(
+            lambda x, y: (self._layers["rescaling"](x), y),
+            num_parallel_calls=config.AUTOTUNE
+        )
+
+        # Optimize performance by caching and prefetching
+        ds = ds.cache().prefetch(config.AUTOTUNE)
+
+        return ds
+
+    def _test_pipeline(self):
+        # Load test set without shuffling
+        ds = image_dataset_from_directory(
+            config.DATA_PATH / "test",
+            labels="inferred",
+            label_mode="int",
+            class_names=config.LABELS,
+            batch_size=config.BATCH_SIZE,
+            image_size=config.IMAGE_SIZE,
+            shuffle=False,
+            crop_to_aspect_ratio=True,
+            data_format="channels_last",
+            verbose=False
+        )
+
+        # Rescale to achieve more stable convergence
+        ds = ds.map(
+            lambda x, y: (self._layers["rescaling"](x), y),
+            num_parallel_calls=config.AUTOTUNE
+        )
+
+        # Optimize performance by caching and prefetching
+        ds = ds.cache().prefetch(config.AUTOTUNE)
+
+        return ds
+
+
+set_random_seed(config.RANDOM_STATE)
+pipeline = Pipeline()
+
+
+def pipeline_smoke_test():
+    for images, labels in pipeline.train_ds.take(1):
+        print(images.numpy().shape, labels.numpy().shape)
+
+
+if __name__ == "__main__":
+    pipeline_smoke_test()
